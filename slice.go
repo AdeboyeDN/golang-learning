a11398ed55d909dad30f42e780dfb88678209971
Daniel <adeboyed93@gmail.com>
2022-07-30 20:04:44 +0300

    learning

---
+++ slice.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func slice() {
+	x := make([]float64, 5, 10)
+	fmt.Println(x)
+
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
+
+	slice3 := []int{1, 2, 3}
+	slice4 := make([]int, 2)
+	copy(slice3, slice4)
+	fmt.Println(slice3, slice4)
+}
